2fa57e233598c7221dbb7180d8523cb3762d9347
Esa Hanikka <esa.hanikka@gmail.com>
2017-10-21 11:20:36 +0300

    a

--- ubuntu/disable-avahi.sh
+++ ubuntu/disable-avahi.sh
@@ -3,9 +3,9 @@ echo
 echo "Change 1 to 0"
 sleep 5
 sudo nano /etc/default/avahi-daemon
-echo 
+echo
 echo Change
 echo "start on (filesystem and started dbus)" to
-echo "start on never and filesystem and started dbus)"
+echo "start on never and filesystem and started dbus)."
 sleep 5
 sudo nano /etc/init/avahi-daemon.conf
