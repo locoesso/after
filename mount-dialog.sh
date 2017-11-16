d7771b2060d23023e1d8bd37ef3e9549442cb888
Esa Hanikka <esa.hanikka@gmail.com>
2017-11-16 14:34:28 +0200

    aaa

--- mount-dialog.sh
+++ mount-dialog.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 input=$(zenity --hide-text --text "Password:" --entry)
-retval=$?
-case $retval in
-0)
+#retval=$?
+#case $retval in
+#0)
 #mkdir -p /home/$USER/esahan;
 #mkdir -p /home/$USER/itpaja;
-#mount –t cifs 10.1.1.35:/homes /home/$USER/esahan –o username=$USER,password=$input,workgroup=workgroup;
+sudo mount //10.1.1.35/esahan /home/esahan/esahan –o username=esahan,password=$input
 #mount –t cifs 10.1.1.35:/itpaja /home/$USER/itpaja –o username=$USER,password=$input,workgroup=workgroup;;
-echo $USER;
-echo $input;;
-1)
-echo "Cancelled.";;
-esac
+#echo $USER;
+echo $input
+#1)
+#echo "Cancelled.";;
+#sac
