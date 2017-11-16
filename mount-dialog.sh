bd739431953efa1815bab7618782db656ca6aab7
Esa Hanikka <esa.hanikka@gmail.com>
2017-11-16 14:48:00 +0200

    asdf

--- mount-dialog.sh
+++ mount-dialog.sh
@@ -5,8 +5,6 @@ input=$(zenity --hide-text --text "Password:" --entry)
 #0)
 #mkdir -p /home/$USER/esahan;
 #mkdir -p /home/$USER/itpaja;
-sudo mount //10.1.1.35/esahan /home/esahan/esahan –o username=esahan,password=$input
-#mount –t cifs 10.1.1.35:/itpaja /home/$USER/itpaja –o username=$USER,password=$input,workgroup=workgroup;;
 #echo $USER;
 echo $input
 #1)
