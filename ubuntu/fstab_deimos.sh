4ce40d487203821d66e301aa7eece2d20a6b2286
locoesso <esa.hanikka@gmail.com>
2018-05-01 13:25:02 +0300

    Fixed cifs mount error on boot

--- ubuntu/fstab_deimos.sh
+++ ubuntu/fstab_deimos.sh
@@ -1,4 +1,9 @@
-#/bin/bash
+#/bin/sh
+
+echo "********************"
+echo "***** USE SUDO *****"
+echo "********************"
+read -p "Press any key..."
 
 sudo apt install cifs-utils
 
@@ -12,38 +17,21 @@ more $HOME/.smbcredentials
 sudo chown root ~/.smbcredentials
 sudo chmod 600 ~/.smbcredentials
 
-echo "Backup fstab"
 now=$(date +"%d-%m-%Y_%H.%M.%S")
 sudo cp /etc/fstab /etc/fstab.$now
 
-echo "Make directories to /media"
-sudo mkdir -p /media/Delta
-sudo mkdir -p /media/Epsilon
-sudo mkdir -p /media/Hadraniel
-sudo mkdir -p /media/Pluto
-sudo mkdir -p /media/Mercury
-sudo mkdir -p /media/Mariner
-sudo mkdir -p /media/Voyager
-# sudo mkdir -p /media/Shared
+sudo mkdir -p /media/Altair
+sudo mkdir -p /media/Betelgeuse
+sudo mkdir -p /media/Canopus
 
-# echo "/dev/disk/by-uuid/d83021bf-5773-4e99-9781-c3ec225befd6 /media/Shared auto nosuid,nodev,nofail,x-gvfs-show,x-gvfs-name=Shared 0 0" | sudo tee -a /etc/fstab
-
-echo "Add lines to fstab"
-echo "//phobos/Delta		/media/Delta		cifs	iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
-echo "//phobos/Epsilon		/media/Epsilon		cifs	iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
-echo "//phobos/Hadraniel	/media/Hadraniel	cifs	iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
-echo "//phobos/Pluto		/media/Pluto		cifs	iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
-echo "//phobos/Mercury		/media/Mercury		cifs	iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
-echo "//phobos/Mariner		/media/Mariner		cifs	iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
-echo "//phobos/Voyager		/media/Voyager		cifs	iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
+echo "//phobos/Altair		/media/Altair		cifs	comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
+echo "//phobos/Betelgeuse	/media/Betelgeuse	cifs	comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
+echo "//phobos/Canopus		/media/Canopus		cifs	comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
 
 echo "Mount filesystems"
 sudo mount -a
 
-echo "Video symlinks"
-ln -s /media/Mercury/Video/ ~/Videos/Mercury
-ln -s /media/Mercury/Video/TV ~/Videos/TV
-ln -s /media/Delta/Video/ ~/Videos/Delta
-ln -s /media/Epsilon/Video/ ~/Videos/Epsilon
-ln -s /media/Hadraniel/Video/ ~/Videos/Hadraniel
-ln -s /media/Pluto/Video/ ~/Videos/Pluto
+ln -s /media/Altair/Video/ ~/Videos/Altair
+ln -s /media/Altair/Video/TV ~/Videos/TV
+ln -s /media/Betelgeuse/Video/ ~/Videos/Betelgeuse
+ln -s /media/Canopus/Video/ ~/Videos/Canopus
