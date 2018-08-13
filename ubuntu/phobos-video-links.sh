b202d5a7f884065e8e0fe41f644d47817bac7516
Esa Hanikka <esa.hanikka@gmail.com>
2018-08-13 13:16:43 +0300

    ...

--- ubuntu/phobos-video-links.sh
+++
@@ -1,3 +0,0 @@
-ln -s /mnt/Altair/Video /home/esso/Videos/Altair
-ln -s /mnt/Altair/Video/TV /home/esso/Videos/TV
-ln -s /mnt/Betelgeuse/Video /home/esso/Videos/Betelgeuse
---
+++ ubuntu/video-links-deimos.sh
@@ -0,0 +1,3 @@
+ln -s /media/Altair/Video $HOME/Videos/Altair
+ln -s /media/Altair/Video/TV $HOME/Videos/TV
+ln -s /media/Betelgeuse/Video $HOME/Videos/Betelgeuse
