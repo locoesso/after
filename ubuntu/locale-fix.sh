d1733fe55554a9d5ea355c2c2b462893f181e7c2
Esa Hanikka <esa.hanikka@gmail.com>
2018-08-17 14:48:27 +0300

    ...

--- ubuntu/locale-fix.sh
+++ ubuntu/locale-fix.sh
@@ -4,4 +4,6 @@ export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 sudo locale-gen en_US.UTF-8
+sudo update-locale LANGUAGE=en_US.UTF-8
+sudo update-locale LANG=en_US.UTF-8
 sudo dpkg-reconfigure locales
