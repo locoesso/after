1083d132a54d48a5e21df5cf9bf524196fce40be
Esa Hanikka <esa.hanikka@gmail.com>
2018-08-22 11:37:02 +0300

    locale fuck up

--- ubuntu/locale-fix.sh
+++ ubuntu/locale-fix.sh
@@ -7,3 +7,6 @@ sudo locale-gen en_US.UTF-8
 sudo update-locale LANGUAGE=en_US.UTF-8
 sudo update-locale LANG=en_US.UTF-8
 sudo dpkg-reconfigure locales
+cat ~/.config/plasma-locale-settings.sh
+echo "VLC language"
+echo "Exec=LANGUAGE=en /usr/bin/vlc %U"
