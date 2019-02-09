1e694467b7503034f68d70a451110f80c13c837a
Esa Hanikka <esa.hanikka@gmail.com>
2019-02-09 11:02:55 +0200

    Fixed names and directories

--- ubuntu/fstab_deimos.sh
+++ ubuntu/fstab_deimos.sh
@@ -10,8 +10,8 @@ sudo apt install cifs-utils smbclient
 echo "Samba user credentials"
 read -p "Username:   " username
 read -p "Password:   " password
-	echo username="$username" >$HOME/.smbcredentials
-	echo password="$password" >>$HOME/.smbcredentials
+	sudo echo username="$username" >$HOME/.smbcredentials
+	sudo echo password="$password" >>$HOME/.smbcredentials
 echo "$HOME/.smbcredentials:"
 more $HOME/.smbcredentials
 sudo chown root ~/.smbcredentials
@@ -20,18 +20,11 @@ sudo chmod 600 ~/.smbcredentials
 now=$(date +"%d-%m-%Y_%H.%M.%S")
 sudo cp /etc/fstab /etc/fstab.$now
 
-sudo mkdir -p /media/Altair
-sudo mkdir -p /media/Betelgeuse
-sudo mkdir -p /media/Backup
+sudo mkdir -p /media/altair
+sudo mkdir -p /media/betelgeuse
 
-
-echo "//phobos/Altair		/media/Altair		cifs	comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
-echo "//phobos/Betelgeuse	/media/Betelgeuse	cifs	comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
-echo "//phobos/Backup	    /media/Backup       cifs	comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
+echo "//phobos/altair		/media/altair			cifs	comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
+echo "//phobos/betelgeuse	/media/betelgeuse		cifs	comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
 
 echo "Mount filesystems"
 sudo mount -a
-
-ln -s /media/Altair/Video/ ~/Videos/Altair
-ln -s /media/Altair/Video/TV ~/Videos/TV
-ln -s /media/Betelgeuse/Video/ ~/Videos/Betelgeuse
