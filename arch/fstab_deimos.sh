e9a0791d1d2544be9b2dd3c0ea388d92d20ea027
Esa Hanikka <esa.hanikka@gmail.com>
2019-02-09 19:44:30 +0200

    ..

--- arch/fstab_deimos.sh
+++ arch/fstab_deimos.sh
@@ -22,9 +22,12 @@ sudo mkdir -p /mnt/apophis
 #sudo mkdir -p /mnt/data
 
 
-echo "//phobos/Altair		/media/altair		cifs	comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
-echo "//phobos/Betelgeuse	/media/betelgeuse	cifs	comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
-echo "/dev/disk/by-uuid/74911197-06fb-442d-aa97-ea63d6f16ea4 /mnt/apophis auto nosuid,nodev,nofail,x-gvfs-show,x-gvfs-name=Apophis 0 0" | sudo tee -a /etc/fstab
+#echo "//phobos/Altair		/media/altair		cifs	 
+comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
+#echo "//phobos/Betelgeuse	/media/betelgeuse	cifs	
+comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
+#echo "/dev/disk/by-uuid/74911197-06fb-442d-aa97-ea63d6f16ea4 /mnt/apophis auto 
+nosuid,nodev,nofail,x-gvfs-show,x-gvfs-name=Apophis 0 0" | sudo tee -a /etc/fstab
 #echo "/dev/disk/by-uuid/10B07312B072FE12 /mnt/Data ntfs-3g nosuid,nodev,nofail,x-gvfs-show,x-gvfs-name=Data 0 0" | sudo tee -a /etc/fstab
 
 echo "Mount filesystems"
