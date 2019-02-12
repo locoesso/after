34a55266fb776fc8d1915ebe75061ace620f5e5d
Esa Hanikka <esa.hanikka@gmail.com>
2019-02-12 08:53:54 +0200

    ...

--- fedora/samba.sh
+++ fedora/samba.sh
@@ -4,15 +4,15 @@ now=$(date +"%d-%m-%Y_%H.%M.%S")
 
 sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.$now
 
-sudo dnf install -y samba
+sudo dnf install samba
 sudo systemctl enable smb nmb
 sudo firewall-cmd --add-service=samba --permanent
 sudo firewall-cmd --reload
 
 # sudo setsebool -P samba_enable_home_dirs on
 
-sudo pdbedit -a locoesso
+sudo pdbedit -a esso
 
-sudo cp smb.conf.phobos /etc/samba/smb.conf
+# sudo cp smb.conf.phobos /etc/samba/smb.conf
 
 sudo systemctl restart smb nmb
