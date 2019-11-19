77f31ca4aa926aa74a544ad862774205386d6e72
locoesso <esa.hanikka@gmail.com>
2019-11-19 23:45:18 +0200

    ...

--- ubuntu/video-links-deimos.sh
+++ ubuntu/video-links-deimos.sh
@@ -1,3 +1,7 @@
 ln -s /media/altair/Video $HOME/Videos/Altair
-ln -s /media/altair/Video/TV $HOME/Videos/TV
+ln -s /media/altair/Video/TV $HOME/Videos/TV-Altair
 ln -s /media/betelgeuse/Video $HOME/Videos/Betelgeuse
+ln -s /media/betelgeuse/Video/TV $HOME/Videos/TV-Betelgeuse
+ln -s /media/deneb/Video $HOME/Videos/Deneb
+ln -s /media/deneb/Video/TV $HOME/Videos/Deneb/TV-Deneb
+
