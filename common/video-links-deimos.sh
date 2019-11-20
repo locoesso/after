228a09ea64a972cc9d7a34e632ebfa649e647ce3
locoesso <esa.hanikka@gmail.com>
2019-11-20 03:02:48 +0200

    ...

--- common/video-links-deimos.sh
+++ common/video-links-deimos.sh
@@ -3,5 +3,5 @@ ln -s /media/altair/Video/TV $HOME/Videos/TV-Altair
 ln -s /media/betelgeuse/Video $HOME/Videos/Betelgeuse
 ln -s /media/betelgeuse/Video/TV $HOME/Videos/TV-Betelgeuse
 ln -s /media/deneb/Video $HOME/Videos/Deneb
-ln -s /media/deneb/Video/TV $HOME/Videos/Deneb/TV-Deneb
+ln -s /media/deneb/Video/TV $HOME/Videos/Deneb-TV
 
