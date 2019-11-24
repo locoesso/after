650bb2d0ef2a585c86b69e87c24ff92f74780730
locoesso <esa.hanikka@gmail.com>
2019-11-25 00:28:17 +0200

    ...

--- screenlayout/phobos/dell.sh
+++ screenlayout/phobos/dell.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI-3 --off --output HDMI-2 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off
+xrandr --output HDMI-1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI-3 --off
