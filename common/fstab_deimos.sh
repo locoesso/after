7d7ea5d6ac342dd24cb6b3ef6d7003c1b69bd919
locoesso <esa.hanikka@gmail.com>
2021-03-25 13:15:29 +0200

    Update fstab_deimos.sh

--- common/fstab_deimos.sh
+++ common/fstab_deimos.sh
@@ -19,19 +19,13 @@ now=$(date +"%d-%m-%Y_%H.%M.%S")
 sudo cp /etc/fstab /etc/fstab.$now
 
 #sudo mkdir -p /mnt/apophis
-sudo mkdir -p /mnt/data
-sudo mkdir -p /media/altair
-sudo mkdir -p /media/betelgeuse
-sudo mkdir -p /media/deneb
-sudo mkdir -p /media/downloads
+#sudo mkdir -p /mnt/data
+sudo mkdir -p /media/becrux
 
 #echo "/dev/disk/by-uuid/74911197-06fb-442d-aa97-ea63d6f16ea4   /mnt/apophis   auto   nosuid,nodev,nofail,x-gvfs-show,x-gvfs-name=Apophis 0 0" | sudo tee -a /etc/fstab
-echo "/dev/disk/by-uuid/10B07312B072FE12                       /mnt/data      ntfs-3g   nosuid,nodev,nofail,x-gvfs-show,x-gvfs-name=Data 0 0" | sudo tee -a /etc/fstab
+#echo "/dev/disk/by-uuid/10B07312B072FE12                       /mnt/data      ntfs-3g   nosuid,nodev,nofail,x-gvfs-show,x-gvfs-name=Data 0 0" | sudo tee -a /etc/fstab
 
-echo "//phobos.local/altair       /media/altair       cifs   comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smb/smbcredentials,uid=1000 0 0" | sudo tee -a /etc/fstab
-echo "//phobos.local/betelgeuse   /media/betelgeuse   cifs   comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smb/smbcredentials,uid=1000 0 0" | sudo tee -a /etc/fstab
-echo "//phobos.local/deneb        /media/deneb        cifs   comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smb/smbcredentials,uid=1000 0 0" | sudo tee -a /etc/fstab
-echo "//phobos.local/downloads    /media/downloads    cifs   comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smb/smbcredentials,uid=1000 0 0" | sudo tee -a /etc/fstab
+echo "//phobos/becrux       /media/becrux       cifs   comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smb/smbcredentials,uid=1000 0 0" | sudo tee -a /etc/fstab
 
 echo "Mount filesystems"
 sudo mount -a
