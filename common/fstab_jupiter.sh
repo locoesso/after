acf08770914a921396b370adcf4da65b91f7cb08
Esa Hanikka <esa.hanikka@gmail.com>
2022-12-04 21:24:48 +0200

    x2314321y45334

--- common/fstab_jupiter.sh
+++ common/fstab_jupiter.sh
@@ -1,16 +1,16 @@
 #/bin/sh
 
-# sudo pacman -S cifs-utils
+sudo pacman -S cifs-utils
 
 echo "Samba user credentials"
 read -p "Username:   " username
 read -p "Password:   " password
-	echo username="$username" >$HOME/.smbcredentials
-	echo password="$password" >>$HOME/.smbcredentials
-echo "$HOME/.smbcredentials:"
-more $HOME/.smbcredentials
-sudo chown root ~/.smbcredentials
-sudo chmod 600 ~/.smbcredentials
+	echo username="$username" >$HOME/.smb/credentials
+	echo password="$password" >>$HOME/.smb/credentials
+echo "$HOME/.smb/credentials:"
+more $HOME/.smb/credentials
+sudo chown root ~/.smb/credentials
+sudo chmod 600 ~/.smb/credentials
 
 echo "Backup fstab"
 now=$(date +"%d-%m-%Y_%H.%M.%S")
@@ -19,9 +19,9 @@ sudo cp /etc/fstab /etc/fstab.$now
 sudo mkdir -p /media/hyperion
 sudo mkdir -p /media/prometheus
 
-echo "//saturn/Hyperion	/media/hyperion    cifs comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
+echo "//saturn/Hyperion	/media/hyperion    cifs comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smb/credentials,uid=1000	0	0" | sudo tee -a /etc/fstab
 
-echo "//saturn/Prometheus /media/prometheus    cifs comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smbcredentials,uid=1000	0	0" | sudo tee -a /etc/fstab
+echo "//saturn/Prometheus /media/prometheus    cifs comment=systemd.automount,iocharset=utf8,credentials=$HOME/.smb/credentials,uid=1000	0	0" | sudo tee -a /etc/fstab
 
 echo "Mount filesystems"
 sudo mount -a
